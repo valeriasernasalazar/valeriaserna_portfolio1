e99947bb1bba2b8d4432ef5efb897604e513bc66
valeriasernasalazar <valeriasernasalazar@icloud.com>
2025-01-08 19:17:39 +0100

    ccc

--- svelte.config.js
+++ svelte.config.js
@@ -2,14 +2,14 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/valeriaserna_portfolio1' : '',
-		  }
-	  }
+  kit: {
+    adapter: adapter({
+      fallback: '404.html',
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/valeriaserna_portfolio1' : '',
+    },
+  },
 };
 
 export default config;
